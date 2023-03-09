8524b7fbc6a9a680a32119dfe63725eae3986539
zineddine <eino1209@gmail.com>
2023-03-09 22:39:46 +0100

    finish

--- rsa/asn1.py
+++ rsa/asn1.py
@@ -1,7 +1,6 @@
 from pyasn1.type import univ, namedtype, tag
 
 
-
 class PubKeyHeader(univ.Sequence):
     componentType = namedtype.NamedTypes(
         namedtype.NamedType("oid", univ.ObjectIdentifier()),
