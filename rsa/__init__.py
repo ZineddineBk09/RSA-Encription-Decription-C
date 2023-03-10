4eff2f6e95782d184a728729116c8d9537cb3205
zineddine <eino1209@gmail.com>
2023-03-10 21:33:37 +0100

    finish

--- rsa/__init__.py
+++ rsa/__init__.py
@@ -1,4 +1,5 @@
 import os
+import time
 from key import newkeys, PrivateKey, PublicKey
 from pkcs1 import (
     encrypt,
@@ -26,8 +27,8 @@ def encrypt_file(file_path, folder_path, pubkey):
         ciphertext = b"".join(encrypt(chunk, pubkey) for chunk in chunks)
 
     with open(folder_path + '/' + os.path.basename(file_path) + ".enc", "wb") as f:
-        print("encrypted file: %s" % folder_path +
-              os.path.basename(file_path) + ".enc")
+        #print("encrypted file: %s" % folder_path +
+              #os.path.basename(file_path) + ".enc")
         f.write(ciphertext)
 
 
@@ -61,7 +62,7 @@ def sign_file(file_path, folder_path, privkey):
 
 # create a function that verify a file and take the path of the file as an argument
 def verify_file(file_path, signature_path, pubkey):
-    print("verifying %s" % file_path)
+    #print("verifying %s" % file_path)
     with open(file_path, "rb") as f:
         message = f.read()
 
@@ -70,26 +71,15 @@ def verify_file(file_path, signature_path, pubkey):
 
     try:
         verify(message, signature, pubkey)
-        print("signature is valid for %s" % file_path)
+        #print("signature is valid for %s" % file_path)
     except VerificationError:
         print("signature is invalid for %s" % file_path)
 
 
 # main2 to encrypt, decrypt and sign a folder of files
 def main():
-    ################## Key generation ##################
-    print('################## Key generation ##################')
-
-    # generate a keypair
-    (pubkey, privkey) = newkeys(1024)
-
-    # log the public  and private keys
-    print("pubkey: %s %s" % (pubkey.n, pubkey.e))
-    print("privkey: %s %s" % (privkey.n, privkey.d))
-
-    ################## Encryption ##################
-    print('################## Encryption ##################')
-
+    ################## User input ##################
+    print('################## User input ##################')
     # get the path of the folder to encrypt
     folder_path = input("Enter the path of the folder to encrypt: ")
 
@@ -105,6 +95,27 @@ def main():
     folder_path_sig = input(
         "Enter the path where the signed files will be stored: ")
 
+    print('######################################################')
+    ################## Key generation ##################
+    print('################## Key generation ##################')
+
+    start = time.time()
+
+    # generate a keypair
+    (pubkey, privkey) = newkeys(1024)
+
+    end = time.time()
+
+    # log the public  and private keys
+    print("pubkey: %s %s" % (pubkey.n, pubkey.e))
+    print("privkey: %s %s" % (privkey.n, privkey.d))
+    print("Time to generate keys: %s" % (end - start)+" sec")
+
+    print('######################################################')
+    ################## Encryption ##################
+    print('################## Encryption ##################')
+
+    start = time.time()
     # get the list of files in the folder
     files_enc = os.listdir(folder_path)
 
@@ -112,36 +123,59 @@ def main():
     for file in files_enc:
         encrypt_file(folder_path + '/' + file, folder_path_enc, pubkey)
 
+    end = time.time()
+    print("Time to encrypt files: %s" % (end - start)+" sec")
+
+    print('######################################################')
     ################## Decryption ##################
     print('################## Decryption ##################')
+    start = time.time()
 
     files_dec = os.listdir(folder_path_enc)
 
     # decrypt each file in the folder
     for file in files_dec:
-        print("Decrypting file: " + file)
+        #print("Decrypting file: " + file)
         decrypt_file(folder_path_enc + '/' + file, folder_path_dec, privkey)
 
+    end = time.time()
+
+    print("Time to decrypt files: %s" % (end - start)+" sec")
+
+    print('######################################################')
     ################## Signing ##################
     print('################## Signing ##################')
 
-    files_sig = os.listdir(folder_path_dec)
+    start = time.time()
+
+    files_sig = os.listdir(folder_path)
 
     # sign each file in the folder
     for file in files_sig:
-        print("Signing file: " + file)
-        sign_file(folder_path_dec + '/' + file, folder_path_sig, privkey)
+        #print("Signing file: " + file)
+        sign_file(folder_path + '/' + file, folder_path_sig, privkey)
 
+    end = time.time()
+
+    print("Time to sign files: %s" % (end - start)+" sec")
+
+    print('######################################################')
     ################## Verification ##################
     print('################## Verification ##################')
+
+    start = time.time()
 
     files_ver = os.listdir(folder_path_sig)
 
     # verify each file in the folder
     for file in files_ver:
-        # pqss file to check without .sig
-        verify_file(folder_path_dec + '/' + file.split('.sig')[0],
+        # pass file to check without .sig
+        verify_file(folder_path + '/' + file.split('.sig')[0],
                     folder_path_sig + '/' + file, pubkey)
+
+    end = time.time()
+
+    print("Time to verify files: %s" % (end - start)+" sec")
 
 
 if __name__ == "__main__":
